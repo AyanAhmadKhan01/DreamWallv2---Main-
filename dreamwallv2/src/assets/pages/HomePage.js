ebbd9474a5a25b742f39fa37efbdbb503253d70c
DevLegend <ayanahmadkhan042@gmail.com>
2025-01-16 05:08:31 +0530

    Half Work Done Home Page

--- dreamwallv2/src/assets/pages/HomePage.js
+++ dreamwallv2/src/assets/pages/HomePage.js
@@ -6,11 +6,11 @@ function Home() {
     return(
         <>
         <Hero />
-        <Features />
-        <Wallpaper/>
+        <BentoBox />
+        {/* <Wallpaper/> */}
         {/* <GettingStarted /> */}
         {/* <ImageSlider /> */}
-        <AboutCards />
+        {/* <AboutCards /> */}
         </>
     )
 };
@@ -32,6 +32,8 @@ function Hero() {
         <div className="hero-btn">
         <button className='hero-btn-gettingstarted'><i className="fas fa-lightbulb"></i>Getting Started</button>
         <button className='hero-btn-explore'><i className="fas fa-layer-group"></i>Explore</button>
+        <div className="circle-dream-wall"></div>  
+        <div className="background-color"></div>  
         </div>
        </div>
        </div>
@@ -39,38 +41,93 @@ function Hero() {
     )
 };
 
-function Features() {
+
+function BentoBox() {
     return(
         <>
-        <div className="feature-container">
-        <h1>Unlock Powerful Features</h1>
-        <p className='feature-paragraph'>Unlock exclusive features like custom bio, profile banner, and unique links as you gain more likes and engage with the DreamWall community!</p>
-        <div className="features-section">
-        <div className="features-card">
-        <i className="fas fa-pencil-alt"></i>
-        <h3>Ability to Edit & Add Bio</h3>
-            <p>Customize your profile by adding and editing your bio</p>
+        <div className="bento-container">
+            <div className="bento-section">
+                <div className="bento-box">
+                <div className="explore-card-home">
+              <img src="/img/Wallpapers/1934580.jpg"/>
+              <div className="leftbar">
+                <div className="leftbar-section">
+                  <h6><i className="far fa-heart"></i></h6>  
+                  <h5><i className="far fa-clone"></i></h5>
+                 </div>
+                <label>Mobile</label>
+                <div className="leftbar-text">
+                  <h2>100 downloads  ● 7 days ago</h2>
+                </div>
+                
+              <h1 className='uploader-name'>Dream Wall</h1>
+            <h3>Space Wallpaper</h3>
+              </div>
+            </div>
+        
+                <div className="explore-card-home">
+              <img src="/img/Wallpapers/Wallpaper 1.jpg" alt="" />
+              <div className="leftbar">
+                <div className="leftbar-section">
+                  <h6><i className="far fa-heart"></i></h6>  
+                  <h5><i className="far fa-clone"></i></h5>
+                 </div>
+                <label>PC</label>
+                <div className="leftbar-text">
+                  <h2>16 downloads  ● 1 days ago</h2>
+                </div>
+                
+              <h1 className='uploader-name'>Dream Wall</h1>
+            <h3>Anime Wallpaper</h3>
+              </div>
+            </div>
+        
+           
+<div className="bento-box-graph">
+<div className="bento-box-graph-section">
+    <div className="analytics">
+        <h1>Analytics <span><i class="fas fa-circle"></i>Live</span></h1>
         </div>
-        <div className="features-card">
-        <i className="fas fa-user-circle"></i>
-            <h3>Custom Profile</h3>
-            <p>Unlock the ability to fully personalize your profile</p>
+        <div className="analytics-box-container">
+        <div className="analytics-box">
+        <h2>TOTAL DOWNLOADS</h2>
+        <h3>10000</h3>
+        <h4>Daily Download <p>52</p></h4>
         </div>
-        <div className="features-card">
-        <i className="fas fa-image"></i>
-            <h3>Custom Banner for Profile</h3>
-            <p>Unlock the ability to fully personalize your profile</p>
-        </div>
-        <div className="features-card">
-        <i className="fas fa-link"></i>
-            <h3>Custom Profile Link</h3>
-            <p>Create a custom link for your profile to share with others</p>
+        <div className="analytics-box">
+        <h2>TOTAL Likes</h2>
+        <h3>100</h3>
+        <h4>Daily Likes <p>99</p></h4>
         </div>
         </div>
+        <h1></h1>
+    </div>
+</div>
+<div className="bento-btn-box">
+    <div className="bento-btn-section">
+                    <i class="fas fa-heart"></i>
+                    <i class="far fa-copy"></i>
+                    </div>
+                    </div>
+                   <div className="bento-profile-box">
+                    <img src="/img/DreamWall-Banner.png" className='bento-banner' />
+                    <img src="/img/dreamwall-logo.png" className='bento-logo' />
+                    <h1>Dream Wall</h1>
+                    <h5>Joined on July 17, 2024</h5>
+                    <div className="bento-profile-option">
+                    <h3>Wallpapers</h3>
+                    <h3>Post</h3>
+                    <h3>About</h3>
+                    </div>
+                    
+                   </div>
+                </div>
+            </div>
         </div>
         </>
     )
-};
+}
+
 
 
 function Wallpaper() {
